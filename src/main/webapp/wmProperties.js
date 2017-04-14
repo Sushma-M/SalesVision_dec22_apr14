8f8f5aa1baf1116d9e5006a6b2005619becf1e8b
root <root@867eade626de>
2017-04-14 11:14:39 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "retro",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_dec22",
+  "homePage" : "Main",
+  "name" : "SalesVision_dec22",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
